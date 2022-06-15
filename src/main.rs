98fa964b16b5389f6cf997a1385d53c531da51b2
Noe Huaccharaque <noemk2@gmail.com>
2022-06-14 19:26:36 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,23 @@ fn main() {
 
 let args: Vec<String> = std::env::args().skip(1).collect();
 
+if args.is_empty() {
+        print_usage_and_exit();
+    }
+
+let subcommand = args.remove(0);
+
+println!("{}", subcommand);
 
 
-}
+match subcommand.as_str() {
+    "convert" => {
+        let input_file = args.remove(0);
+        }
+    }// match
+
+
+
+
+
+}//main
