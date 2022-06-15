d2907bce2fb476cdc60c2eed69b49ca9a18c062e
Noe <noemk2@gmail.com>
2022-06-15 14:33:42 +0000

    rotate method

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub fn blur(infile: &String, outfile: &String) {
     img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
-pub fn fractal(outfile: String) {
+pub fn fractal(outfile: &String) {
     let width = 800;
     let height = 800;
 
@@ -53,44 +53,46 @@ pub fn fractal(outfile: String) {
     imgbuf.save(outfile).unwrap();
 }
 
-pub fn brighten(infile: String, outfile: String) {
+pub fn brighten(infile: &String, outfile: &String) {
     // See blur() for an example of how to open / save an image.
-
+    let img = image::open(infile).expect("Failed to open INFILE.");
     // .brighten() takes one argument, an i32.  Positive numbers brighten the
     // image. Negative numbers darken it.  It returns a new image.
-
+    let img2 = img.brighten(10);
     // Challenge: parse the brightness amount from the command-line and pass it
     // through to this function.
+    img2.save(outfile).expect("Failed writing OUTFILE.");
 }
 
-pub fn crop(infile: String, outfile: String) {
+pub fn crop(infile: &String, outfile: &String) {
     // See blur() for an example of how to open an image.
-
+    let img = image::open(infile).expect("Failed to open INFILE.");
     // .crop() takes four arguments: x: u32, y: u32, width: u32, height: u32
     // You may hard-code them, if you like.  It returns a new image.
-
+    let img2 = img.clone().crop( 100, 100, 200, 200);
     // Challenge: parse the four values from the command-line and pass them
     // through to this function.
-
+    img2.save(outfile).expect("Failed writing OUTFILE.");
     // See blur() for an example of how to save the image.
 }
 
-pub fn rotate(infile: String, outfile: String) {
+pub fn rotate(infile: &String, outfile: &String) {
     // See blur() for an example of how to open an image.
-
+    let img = image::open(infile).expect("Failed to open INFILE.");
     // There are 3 rotate functions to choose from (all clockwise):
     //   .rotate90()
     //   .rotate180()
     //   .rotate270()
     // All three methods return a new image.  Pick one and use it!
+    let img2 = img.rotate180();
 
     // Challenge: parse the rotation amount from the command-line, pass it
     // through to this function to select which method to call.
-
+    img2.save(outfile).expect("Failed writing OUTFILE.");
     // See blur() for an example of how to save the image.
 }
 
-pub fn invert(infile: String, outfile: String) {
+pub fn invert(infile: &String, outfile: &String) {
     // See blur() for an example of how to open an image.
 
     // .invert() takes no arguments and converts the image in-place, so you
@@ -99,7 +101,7 @@ pub fn invert(infile: String, outfile: String) {
     // See blur() for an example of how to save the image.
 }
 
-pub fn grayscale(infile: String, outfile: String) {
+pub fn grayscale(infile: &String, outfile: &String) {
     // See blur() for an example of how to open an image.
 
     // .grayscale() takes no arguments. It returns a new image.
@@ -107,7 +109,7 @@ pub fn grayscale(infile: String, outfile: String) {
     // See blur() for an example of how to save the image.
 }
 
-pub fn generate(outfile: String) {
+pub fn generate(outfile: &String) {
     // Create an ImageBuffer -- see fractal() for an example
 
     // Iterate over the coordinates and pixels of the image -- see fractal() for an example
@@ -134,19 +136,51 @@ pub fn generate_options(processing: &String, mut args: Vec<String>) {
             let infile = args.remove(0);
             let outfile = args.remove(0);
             // **OPTION**
-            // println!("{:?}", infile);
-            // println!("{:?}", outfile);
             // Improve the blur implementation -- see the blur() function below
             blur(&infile, &outfile);
         }
-        
+
         "fractal" => {
             if args.len() != 1 {
                 print_usage_and_exit();
             }
             let outfile = args.remove(0);
-            fractal(outfile);
+            fractal(&outfile);
         }
+
+        "brighten" => {
+            if args.len() != 2 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            // **OPTION**
+            // Improve the brightness implementation -- see the brighten() function below
+            brighten(&infile, &outfile);
+        }
+
+        "crop" => {
+            if args.len() != 2 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            // **OPTION**
+            // Improve the crop implementation -- see the crop() function below
+            crop(&infile, &outfile);
+        }
+
+        "rotate" => {
+            if args.len() != 2 {
+                print_usage_and_exit();
+            }
+            let infile = args.remove(0);
+            let outfile = args.remove(0);
+            // **OPTION**
+            // Improve the rotate implementation -- see the rotate() function below
+            rotate(&infile, &outfile);
+        }
+
         _ => {
             print_usage_and_exit();
         }
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,21 @@
 // TITLE: procesador de imagenes
-#![allow(unused_variables, dead_code, unused_imports)]
+// #![allow(unused_variables, dead_code, unused_imports)]
 
-use variables::{blur, fractal, generate_options, print_usage_and_exit};
+use variables::{generate_options, print_usage_and_exit};
 
 //  cargo run --release blur image.png blurred.png blur.png
-//  cargo run --release blur image.png 
+//  cargo run --release blur image.png
 
 fn main() {
     // use clap future
     let mut args: Vec<String> = std::env::args().skip(1).collect();
-    // assert_eq!(args.is_empty(), vec![""].is_empty());
-
-    // cargo run --release blur image.png blurred.png
-    // println!("this is args: {:?}", args);
-
     // assert!(args.is_empty()); // wtf
     if args.is_empty() {
         print_usage_and_exit();
     }
 
-    // fix : using two variables
     // let processing= args.clone().remove(0);
-    let processing= args.remove(0);
+    let processing = args.remove(0);
 
-    // vefificar
-    // println!("this is process: {}", processing); // arg.1
     generate_options(&processing, args);
-
-} //main
+} 
